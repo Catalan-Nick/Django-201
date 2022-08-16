8beeda3ecfb07dec44f0da1f5d0c04fcf79047f0
Catalan-Nick <catalan1222@yahoo.com>
2022-08-16 04:12:29 -0500

    Moved homepage, changed dir

--- feed/views.py
+++ feed/views.py
@@ -4,7 +4,7 @@ from .models import Post
 
 class HomeView(ListView):
     http_method_names = ["get"]
-    template_name = "homepage.html"
+    template_name = "feed/homepage.html"
     model = Post
     context_object_name = "posts"
-    queryset - Post.objects.all().order_by('-id')[0:30]
+    queryset = Post.objects.all().order_by('-id')[0:30]
