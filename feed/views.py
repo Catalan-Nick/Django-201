a850fb8e12a6d87a2aa8090ce10642086af86c0a
Catalan-Nick <catalan1222@yahoo.com>
2022-08-27 16:37:43 -0500

    New new post modal

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import ListView
-
+import re
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render 
 from .models import Post
 
 class HomeView(ListView):
@@ -8,3 +11,41 @@ class HomeView(ListView):
     model = Post
     context_object_name = "posts"
     queryset = Post.objects.all().order_by('-id')[0:30]
+    
+class PostDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name = "feed/detail.html"
+    model = Post
+    context_object_name = "post"
+    
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "feed/create.html"
+    fields = ['text']
+    success_url = "/"
+    
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+        obj.save()
+        return super().form_valid(form)
+    
+    def post(self, request, *args, **kwargs):
+        
+        post = Post.objects.create(
+            text=request.POST.get("text"),
+            author=request.user, 
+        )
+        return render(
+            request,
+            "includes/post.html",
+            {
+                "post": post,
+                "show_detail_link": True,
+            },
+            content_type="application/html"
+            )
