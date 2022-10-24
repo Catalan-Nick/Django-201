bb5a522127554f0c610c5cc9ed772a71e78ae7bd
Catalan-Nick <catalan1222@yahoo.com>
2022-10-23 22:03:17 -0500

    Added demo password

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.views.generic import DetailView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse, HttpResponseBadRequest
-
+from followers.models import Follower
 from feed.models import Post
 class ProfileDetailView(DetailView):
     http_method_names = ["get"]
@@ -13,11 +13,18 @@ class ProfileDetailView(DetailView):
     slug_field = "username"
     slug_url_kwarg = "username"
     
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+    
+    
     def get_context_data(self, **kwargs):
         user = self.get_object()
         context = super().get_context_data(**kwargs)
         context['total_posts'] = Post.objects.filter(author=user).count
         # context['total_followers'] = ...#
+        if self.request.user.is_authenticated:
+            context['you_follow'] = Follower.objects.filter(following=user, followed_by=self.request.user).exists()
         return context
     
 class FollowView(LoginRequiredMixin, View):
@@ -33,6 +40,26 @@ class FollowView(LoginRequiredMixin, View):
             other_user  = User.objects.get(username=data['username'])
         except User.DoesNotExist:
             return HttpResponseBadRequest("Missing user")
+        
+        if data['action'] == "follow":
+            #follow
+            follower, created = Follower.objects.get_or_create(
+                followed_by=request.user,
+                following=other_user
+            )
+        else:
+            #unfollow
+            try:
+                follower = Follower.objects.get(
+                    followed_by=request.user,
+                    following=other_user
+                )
+            except Follower.DoesNotExist:
+                follower = None
+            
+            if follower:
+                follower.delete()
         return JsonResponse({
-            'done': True,
+            'sucess': True,
+            'wording': "Unfollow" if data['action'] == "follow" else "Follow"
         })
